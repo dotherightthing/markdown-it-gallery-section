e8a18765c8c760773264a940dfc3cb2aa4f362c7
Dan Smith <dev@dotherightthing.co.nz>
2023-01-15 15:48:58 +1300

    Initial commit

---
+++ README.md
@@ -0,0 +1,116 @@
+# markdown-it-vuepress-gallery
+
+A [markdown-it](https://github.com/markdown-it/markdown-it) plugin for wrapping a sequence of images with a custom Vue component. For use with Vuepress.
+
+I find this preferable to using a Vue component directly in the Vuepress markdown file. Images are core content and should be visible when editing content in a pluggable IDE without requiring Vuepress for compilation.
+
+Based on <https://github.com/amokrushin/markdown-it-gallery>.
+
+## Usage
+
+### Options
+
+| Option                    | Type    | Default   | Description                                                                                 |
+|---------------------------|---------|-----------|---------------------------------------------------------------------------------------------|
+| title                     | String  | ""        | Title to pass to the Vue Component                                                          |
+| titleFromPrecedingHeading | Boolean | true      | Reuse the title from the preceding sibling heading element (rather than providing a string) |
+| vueGalleryTag             | String  | "Gallery" | Name of the Vue component (authored separately)                                             |
+
+### Example
+
+```js
+// .vuepress/config.js
+
+module.exports = {
+  markdown: {
+    extendMarkdown: md => {
+      md.use(require('markdown-it-vuepress-gallery'))
+    }
+  }
+}
+```
+
+```vue
+// .vuepress/components/Gallery.vue (simplified example)
+
+<template>
+    <div class="gallery" :id="'gallery-' + id">
+        <h2 :id="titleId">{{ title }}</h2>
+        <div class="gallery-items">
+            <GalleryItem v-for="(item,key) in images"
+                :src="item.src"
+                :alt="item.alt"
+                :galleryId="id"
+                :id="item.id"
+                :key="key"
+            />
+        </div>
+    </div>
+</template>
+
+<script>
+import GalleryItem from "./GalleryItem";
+
+export default {
+  computed: {
+    titleId() {
+      return this.stringToId(this.title);
+    }
+  },
+  methods: {
+    stringToId: function(str) {
+      return str
+        .trim()
+        .toLowerCase()
+        .replaceAll(/([ /.,'"!()])+/g, '-')
+        .replaceAll(/[-]{2,}/g, '-');
+    }
+  },
+  props: {
+    id: String,
+    images: Array,
+    title: String
+  },
+}
+</script>
+```
+
+```vue
+// .vuepress/components/GalleryItem.vue (simplified example)
+
+<template>
+    <img :src="src" class="gallery-image" :alt="alt" :id="'gallery-' + galleryId + '-image-' + id" width="300" height="300">
+</template>
+
+<script>
+export default {
+  props: {
+    alt: String,
+    galleryId: String,
+    id: String,
+    src: String
+  }
+}
+</script>
+```
+
+#### Input markdown
+
+```md
+## Heading 1
+
+![Gallery Image 1](http://example.com/image-1.jpg)
+![Gallery Image 2](http://example.com/image-2.jpg)
+```
+
+#### Output HTML
+
+```html
+<div class="gallery" id="gallery-0">
+    <h2 id="heading-1">Heading 1</h2>
+    <div class="gallery-items">
+        <img src="http://example.com/image-1.jpg" class="gallery-image" alt="Gallery Image 1" id="gallery-0-image-0" width="300" height="300">
+        <img src="http://example.com/image-2.jpg" class="gallery-image" alt="Gallery Image 2" id="gallery-0-image-1" width="300" height="300">
+    </div>
+</div>
+```
---
+++ index.js
@@ -0,0 +1,105 @@
+/**
+ * @file index.js
+ * @summary markdown-it-vue-gallery, based on markdown-it-gallery
+ * @description A markdown-it plugin for wrapping a sequence of images with a custom Vue component.
+ */
+
+class VueGalleryPlugin {
+    constructor(md, options) {
+        this.md = md;
+        this.options = Object.assign({
+          title: '',
+          titleFromPrecedingHeading: true,
+          vueGalleryTag: 'Gallery',
+        }, options);
+
+        md.core.ruler.after('inline', 'gallery', this.gallery.bind(this));
+    }
+
+    gallery(state) {
+        const tokens = state.tokens;
+        const CHILD_TYPES = ['image', 'softbreak'];
+        let galleryCount = 0;
+
+        for (let i = 1; i < tokens.length - 1; i++) {
+            const token = tokens[i];
+
+            if (token.type !== 'inline') {
+                continue;
+            }
+
+            if (!token.children.length) {
+                continue;
+            }
+
+            // children tokens are only of type image and softbreak
+            if (!token.children.reduce((acc, t) => acc && CHILD_TYPES.includes(t.type), true)) {
+                continue;
+            }
+
+            // prev token is paragraph open
+            if (tokens[i - 1].type !== 'paragraph_open') {
+                continue;
+            }
+
+            // next token is paragraph close
+            if (!tokens[i + 1] || tokens[i + 1].type !== 'paragraph_close') {
+                continue;
+            }
+
+            const children = token.children.filter(t => t.type === 'image');
+
+            // preceding element must be a heading in order to reuse the heading text within the gallery
+            if (this.options.titleFromPrecedingHeading) {
+                if (tokens[i - 2].type !== 'heading_close') {
+                    continue;
+                }
+            }
+
+            // id
+            const id = galleryCount;
+
+            galleryCount += 1;
+
+            // images
+            const images = [];
+
+            children.forEach((child, key) => {
+                images.push({
+                    alt: child.content,
+                    id: key,
+                    src: child.attrGet('src')
+                });
+            });
+
+            // title
+            let title = this.title;
+
+            if (this.options.titleFromPrecedingHeading) {
+                title = tokens[i - 3].content;
+
+                if (tokens[i - 4].type === 'heading_open') {
+                    // note: generated id (anchor) attribute is not present when this plugin runs
+                    // it is generated in the Vue component
+                    tokens[i - 4].attrSet('hidden', 'hidden');
+                }
+            }
+
+            // https://stackoverflow.com/a/64049986
+            let imagesStr = JSON.stringify(images);
+            imagesStr = imagesStr.replace(/"([^"]+)":/g, '$1:'); // unquote property keys
+            imagesStr = imagesStr.replaceAll("'", "\'"); // escape single quotes in content
+            imagesStr = imagesStr.replaceAll("\"", "'"); // use single quotes around property values
+
+            // repurpose paragraph_open token
+            tokens[i - 1].content = `<${vueGalleryTag} id="${id}" title="${title}" :images="${imagesStr}"/>`;
+            tokens[i - 1].type = 'html_block';
+
+            // repurpose paragraph_close token
+            tokens[i + 1].content = '';
+            tokens[i + 1].type = 'html_block';
+        }
+    }
+}
+
+module.exports = (md, options = {}) => new VueGalleryPlugin(md, options);
